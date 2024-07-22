1c856b7126010cb3cad45c6ae5303e58a4fd9ae6
Maureen Nyanamba <maureenmoraa006@gmail.com>
2024-07-22 17:57:30 +0300

    Modified

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  word = word.toLowerCase();
+  let left = 0;
+  let right = word.length - 1;
+
+  while (left < right) {
+    if (word[left] !== word[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
 }
 
 /* 
